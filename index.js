f195542cef77d1265c07a97ec92f042c2edc6f5d
MarioJr77 <mlgj77@yahoo.com.br>
2023-09-07 11:12:56 -0300

    projeto logica dio

---
+++ index.js
@@ -0,0 +1,24 @@
+// area de variaveis
+let nomeHeroi = 'Batman';
+let xpHeroi = 4455;
+let nivelHeroi = '';
+
+if (xpHeroi < 1000) {
+    nivelHeroi = 'Ferro';
+} else if (xpHeroi <= 2000) {
+    nivelHeroi = 'Bronze';
+} else if (xpHeroi <= 5000) {
+    nivelHeroi = 'Prata';
+} else if (xpHeroi <= 7000) {
+    nivelHeroi = 'Ouro';
+} else if (xpHeroi <= 8000) {
+    nivelHeroi = 'Platina';
+} else if (xpHeroi <= 9000) {
+    nivelHeroi = 'Ascendente';
+} else if (xpHeroi <= 10000) {
+    nivelHeroi = 'Imortal';
+} else {
+    nivelHeroi = 'Radiante';
+}
+
+console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivelHeroi}.`);
